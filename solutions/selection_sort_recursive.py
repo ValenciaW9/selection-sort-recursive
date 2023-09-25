2fdcfc6b07170abc19c178b4c3dec03e1e1eac88
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-25 18:06:43 -0400

    selection-sort-recursive/
    selection-sort-recursive/

--- solutions/selection_sort_recursive.py
+++ solutions/selection_sort_recursive.py
@@ -1,102 +1,94 @@
-def selection_sort_recursive(list):
+function selectionSortRecursive(arr) {
+  if (arr.length === 0) {
+    return [];
+  }
 
-    if len(list) == 0:
-        return []
-    
-    minElement = min(list)
+  const min = Math.min(...arr);
+  const idx = arr.indexOf(min);
+  arr.splice(idx, 1);
 
-    list.remove(minElement)
+  const result = selectionSortRecursive(arr);
+  result.unshift(min);
+  return result;
+}
 
-    result = selection_sort_recursive(list)
+if (require.main === module) {
+  // add your own tests in here
+  console.log("Expecting: [-1, 2, 3, 5]");
+  console.log(selectionSortRecursive([3, -1, 5, 2]));
 
-    result.insert(0, minElement)
+  console.log("");
 
-    return result
+  console.log("Expecting: [5]");
+  console.log(selectionSortRecursive([5]));
 
-if __name__ == '__main__':
-    print("Expecting: [-1, 2, 3, 5]")
-    print(selection_sort_recursive([3, -1, 5, 2]))
+  console.log("");
 
-    print("")
+  console.log("Expecting: [-1, 2, 2, 3, 3, 5]");
+  console.log(selectionSortRecursive([3, 2, -1, 3, 5, 2]));
 
-    print("Expecting: [5]")
-    print(selection_sort_recursive([5]))
+  console.log("");
 
-    print("")
+  console.log("Expecting: [3, 5]");
+  console.log(selectionSortRecursive([5, 3]));
 
-    print("Expecting: [-1, 2, 2, 3, 3, 5]")
-    print(selection_sort_recursive([3, 2, -1, 3, 5, 2]))
+  console.log("");
+}
 
-    print("")
+module.exports = selectionSortRecursive;
+const quickSort = (arr) => {
+  if(arr.length <= 1) {
+    return arr ;
+  }
 
-    print("Expecting: [3, 5]")
-    print(selection_sort_recursive([5, 3]))
+  const pivotIndex = Math.floor(Math.random() * arr.length);
+  const pivot = arr[pivotIndex];
+  const leftArr = [];
+  const rightArr = [];
 
-    print("")
+  for (let i = 0; i < arr.length; i++) {
+    if (i === pivotIndex) {
+      continue; // Skip the pivot element
+    }
 
+    if (arr[i] < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
+    }
+  }
 
-# Output: [1, 2, 3, 4, 5, 7, 8]
-
-
-
-
-
-
-def merge_sort(my_list)
-    # Base Case
-    if len(my_list) <= 1:
-        return my_list
-   
-    list_1 = my_list[0:len(my_list) // 2]
-    list_2 = my_list[len(my_list) // 2:]
-    
-    # Induction Step
-    ans_1 = merge_sort(list_1)
-    ans_2 = merge_sort(list_2)
-    
-    # Sorting and merging two sorted list
-    sort_list = sort_two_list(ans_1, ans_2)
-    return sort_list
-
-
-
-# Separate Function to sort and merge 2 sorted lists
-def sort_two_list(list_1, list_2):
-    final_list = []
-    i = 0
-    j = 0
-    while i < len(list_1) and j < len(list_2):
-        if list_1[i] <= list_2[j]:
-            final_list.append(list_1[i])
-            i += 1
-            continue
-        final_list.append(list_2[j])
-        j += 1
-
-    while i < len(list_1):
-        final_list.append(list_1[i])
-        i = i + 1
-        
-    while j < len(list_2):
-        final_list.append(list_2[j])
-        j = j + 1
-        
-    return final_list
+  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+}
 
 
-my_list = [3, 8, 2, 7, 1, 4, 5]
-ans = merge_sort(my_list)
-print(ans)
-# prints [1, 2, 3, 4, 5, 7, 8}
+#return empty array if array is empty
+#}
+/**
+ #The quickSort function sorts an array using the quicksort algorithm recursively.
+ #param arr - The parameter `arr` is an array of elements that you want to sort using the quicksort
+ #algorithm.
+ ##returnsThe quickSort function returns a sorted array.
+ #find smallest value in array and store it in min
+##find index of smallest value and store it in idx
+#// remove the smallest value from the array
+#
+#recurse through the array and store the result in result
+##place the min from each frame at the front of the array* return result
 
 
+##And a written explanation of your solution
+#Once again, it's easier to start with the base case: if the array is empty, return
+#an empty array. We can then add items to this returned empty array in the previous
+#frames. To figure out what to add, we need to find the minimum value in the array
+#and its index. Next, we remove that value from the array and pass the array to the
+#recursive call. This means that #as we go deeper and deeper into the stack, the
+#array gets smaller, until it's empty. Since frames are completed in last in first out
+#order, we need to add the minimum value from each frame to the front of the result
+#array. For example, if the array is [2, 1], the minimum in the first frame is 1, and
+#the recusrive call is made with the argument [2]. In this frame the minimum is 2, so
+#the recursive call is now made with the argument []. This hits the base case, which
+#returns []. result is now an empty array in the previous frame. On the next line, the
+#minimum, which is 2, gets unshifted on. [2] is returned up the stack, and stored in
+# result. Here, the minimum is 1, which gets unshifted on, resulting in [1, 2].
 
-##
-    #The `merge_sort` function takes a list as input and recursively divides it into smaller sublists
-   #until each sublist contains only one element, then merges and sorts the sublists to return a sorted
-    #list.
-    
-    #param my_list: The `my_list` parameter is a list of integers that you want to sort using the merge
-    #sort algorithm. In the example provided, `my_list` is `[3, 8, 2, 7, 1, 4, 5]`
-    #:return: The merge_sort function returns a sorted version of the input list.
-  #####
